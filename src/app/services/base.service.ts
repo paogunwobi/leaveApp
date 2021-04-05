9f28652b47515398d19e9fcaf5c9579044d2d91b
Philip Ogunwobi <paogunwobi@gmail.com>
2021-04-05 01:17:42 +0100

    updates

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class BaseService {
-  baseUrl = 'https://jsonplaceholder.typicode.com/';
+  baseUrl = 'https://leave-app-be.herokuapp.com/';
 
   controllerName = '';
 
